d6f85c815181ada3a9ef989e6d10a62c56ea1c8b
Filfeni <47361476+Filfeni@users.noreply.github.com>
2020-11-17 17:50:43 -0400

    Code
    Added incomplete source code

---
+++ Xamarin_UI_Navigation/Xamarin_UI_Navigation/Home.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_UI_Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : TabbedPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private async void SignIn(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(emailEntry.Text) || string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                await DisplayAlert("Error", "Email or Password fields can't be empty", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Welcome", $"Hello {emailEntry.Text}", "Ok");
+            }
+        }
+
+        private void Register(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
